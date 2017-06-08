18f4adb430bebec168ea6c04ddeae9febf953f79
zhengheyang <zhenghe_yang@hotmail.com>
2017-06-09 00:30:34 +0800

    practice

--- JavaScript DOM编程艺术/scripts/pseudoprotocol.js
+++ JavaScript DOM编程艺术/scripts/pseudoprotocol.js
@@ -1,3 +1,17 @@
-function newOpen (url) {
-  window.open(url,"example","windth=320,height=480");
+function newOpen(url) {
+  window.open(url, "example", "windth=320,height=480");
 }
+
+
+window.onload = function() {
+  if (!document.getElementsByTagName) return false;
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].getAttribute("class") == "popup") {
+      links[i].onclick = function() {
+        newOpen(this.getAttribute("href"));
+        return false;
+      }
+    }
+  }
+};
--- JavaScript DOM编程艺术/scripts/showPic.js
+++ JavaScript DOM编程艺术/scripts/showPic.js
@@ -1,14 +1,45 @@
 /**
  * Created by zhyang on 17-3-9.
  */
+addLoadEvent(prepareGallery);
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById('imagegallery')) return false;
+    var gallery = document.getElementById('imagegallery');
+    var links = gallery.document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclik = function() {
+            return !showPic(this);
+        }
+    }
+
+}
 
 function showPic(whichpic) {
-    var source=whichpic.getAttribute("href");
-    var placeholder=document.getElementById("placeholder");
-    placeholder.setAttribute("src",source);
-    var text=whichpic.getAttribute("title");
-    var description=document.getElementById("description");
-    description.firstChild.nodeValue=text;
+    if (!document.getElementById('placeholder')) return false;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src", source);
+    if (document.getElementById('description')) {
+        var text = whichpic.getAttribute("title");
+        var description = document.getElementById("description");
+        description.firstChild.nodeValue = text;
+    }
+    return true;
 }
 
 // function countBodyChildren() {
